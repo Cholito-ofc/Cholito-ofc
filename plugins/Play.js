13d719e73f8ee77c810478a3a1dd360ee4ca2c77
Cholito-ofc <onanposadas7@gmail.com>
2025-06-27 07:45:40 -0600

    Actualizar Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -109,13 +109,12 @@ const handler = async (msg, { conn, args }) => {
 > ® ⍴᥆ᥕᥱrᥱძ ᑲᥡ 𝖪𝗂𝗅𝗅𝗎𝖺𝖡𝗈𝗍⚡`;
 
     await conn.sendMessage(chatId, {
-  image: imageBuffer,
-  caption: caption,
   contextInfo: {
     externalAdReply: {
       title: title,
       body: 'KilluaBot 🎶',
      thumbnail: imageBuffer,
+     caption: caption,
       mediaType: 1,
       renderLargerThumbnail: true,
       sourceUrl: videoUrl
