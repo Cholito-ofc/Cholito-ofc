5e60f52134bf1d2f151244259e176f4296a839c7
Cholito-ofc <onanposadas7@gmail.com>
2025-07-03 14:08:23 -0600

    Actualizar Pack.js

--- plugins/Pack.js
+++ plugins/Pack.js
@@ -102,7 +102,7 @@ const handler = async (msg, { conn }) => {
   }
 };
 
-handler.command = ["pornololi"];
+handler.command = ["loli"];
 handler.tags = ["nsfw"];
 handler.help = ["pornololi"];
 module.exports = handler;
